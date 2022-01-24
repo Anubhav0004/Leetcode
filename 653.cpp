fc4f3903fd4d36bc7898991cb5551ffdbf32211e
Anubhav0004 <anubhav.goyal1029@gmail.com>
2022-01-24 05:55:47 +0530

    TREE

---
+++ 653.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) 
+    {
+        if(!root->left && !root->right)
+            return false;
+        unordered_map<int,int> mp;
+        queue<TreeNode *> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* temp=q.front();
+            q.pop();
+            mp[temp->val]++;
+            if(temp->left)
+                q.push(temp->left);
+            if(temp->right)
+                q.push(temp->right);
+        }
+        for(auto it=mp.begin();it!=mp.end();it++)
+        {
+            int val=it->first;
+            if(k-val!=val && mp.count(k-val))
+                return true;
+        }
+        return false;
+    }
+};
+/////////////////////////////////////////////////ALTERNATE APPROACH///////////////////////////////////////////////////////
+class Solution {
+public:
+    void solve(TreeNode *root,vector<int> &v)
+    {
+        if(!root)
+            return;
+        solve(root->left,v);
+        v.push_back(root->val);
+        solve(root->right,v);
+    }
+    bool findTarget(TreeNode* root, int k) 
+    {
+        if(!root->left && !root->right)
+            return false;
+        vector<int> v;
+        solve(root,v);
+        for(int i=0;i<v.size()-1;i++)
+        {
+            for(int j=i+1;j<v.size();j++)
+            {
+                if(k-v[i]==v[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+};
