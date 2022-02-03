f268456a575681c17f7109ffd6025d2271c9a4be
Anubhav0004 <anubhav.goyal1029@gmail.com>
2022-02-04 04:36:29 +0530

    TREE

---
+++ 222.cpp
@@ -0,0 +1,9 @@
+class Solution { ////////////////////////////////////////------siraa laya :)-----------------/////////////////////////////////////////////////////////////
+public:
+    int countNodes(TreeNode* root) 
+    {
+        if(!root)
+            return 0;
+        return 1+countNodes(root->left)+countNodes(root->right);
+    }
+};
