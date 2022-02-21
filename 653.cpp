2bf914705f186e8be864e67943b16891526d9067
Anubhav0004 <anubhav.goyal1029@gmail.com>
2022-02-22 01:38:26 +0530

    TREE

--- 653.cpp
+++ 653.cpp
@@ -1,3 +1,36 @@
+////////////////////////////////////////////////////EFFICIENT////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////ALTERNATE/////////////////////////////////////////
+void solve(TreeNode *root,vector<int> &v)
+    {
+        if(!root)
+            return;
+        solve(root->left,v);
+        v.push_back(root->val);
+        solve(root->right,v);
+    }
+    bool findTarget(TreeNode* root, int k) 
+    {
+        if(!root->left && !root->right)
+            return false;
+        vector<int> v;
+        solve(root,v);
+        int i=0;
+        int j=v.size()-1;
+        while(true)
+        {
+            if(i>=j)
+                return false;
+            if(v[i]+v[j]==k)
+                return true;
+            else if(v[i]+v[j]<k)
+                i++;
+            else
+                j--;
+        }
+        return false;
+    }
+//////////////////////////////////////////////////////////ALTERNATE////////////////////////////////////////////////////////////////
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) 
