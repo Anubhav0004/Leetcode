bb1aeeeee2740a11bac93c0aa431c7038a07385a
Anubhav0004 <anubhav.goyal1029@gmail.com>
2022-05-22 03:36:58 +0530

    STRING

---
+++ 1347.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minSteps(string s, string t) 
+    {
+        unordered_map<char,int> mp1;
+        unordered_map<char,int> mp2;
+        int add=0,sub=0;
+        for(int i=0;i<s.size();i++)
+        {
+            mp1[s[i]]++;
+            mp2[t[i]]++;
+        }
+        for(auto it:mp1)
+        {
+            if(mp2.find(it.first)!=mp2.end())
+            {
+                int diff=mp2[it.first]-it.second;
+                if(diff>0)
+                    sub+=diff;
+                else if(diff<0)
+                    add+=abs(diff);
+            }
+            else
+            {
+                add+=it.second;
+            }
+        }
+        for(auto it:mp2)
+        {
+            if(mp1.find(it.first)==mp1.end())
+                sub+=it.second;
+        }
+        if(sub==add)
+        return sub;
+        return abs(sub-add);
+    }
+};
