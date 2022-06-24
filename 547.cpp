2a0d592efb4ff2aef8f4d3fc7535653cca5df221
Anubhav0004 <anubhav.goyal1029@gmail.com>
2022-06-25 03:45:15 +0530

    GRAPH

---
+++ 547.cpp
@@ -0,0 +1,41 @@
+class Solution { //////////////number of connected components of graph////////////////
+public:
+    
+    void dfs(int node,vector<int> &vis,vector<int> adj[])
+    {
+        vis[node]=1;
+        for(auto it:adj[node])
+        {
+            if(!vis[it])
+                dfs(it,vis,adj);
+        }
+    }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) 
+    {
+        int n=isConnected.size();
+        int m=isConnected[0].size();
+        vector<int> adj[n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(isConnected[i][j]==1)
+                {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+        vector<int> vis(n,0);
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                cnt++;
+                dfs(i,vis,adj);
+            }
+        }
+        return cnt;
+    }
+};
