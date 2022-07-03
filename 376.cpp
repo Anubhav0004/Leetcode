fd549b51d2fa755ef3a961c842596cca80da3456
Anubhav0004 <anubhav.goyal1029@gmail.com>
2022-07-04 01:40:42 +0530

    DP

--- 376.cpp
+++ 376.cpp
@@ -2,6 +2,18 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) 
     {
+        /////////////////////////DP---100%//////////////////
+        int n = nums.size();
+        int peak = 1, valley = 1;
+        
+        for(int i=1; i<n; i++){
+            if(nums[i]>nums[i-1])
+                peak=valley + 1;
+            else if(nums[i]<nums[i-1])
+                valley = peak + 1;
+        }
+        return max(peak, valley);
+        /////////Greedy/////////////////
         if(nums.size()<2)
             return nums.size();
         int prev=nums[1]-nums[0];
