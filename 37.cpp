40381cbb9cbe04a4ee7aa1bb52cecef37e202bfd
Anubhav0004 <anubhav.goyal1029@gmail.com>
2022-07-15 06:46:58 +0530

    BACKTRACKING

---
+++ 37.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool valid(int row,int col,char c,vector<vector<char>> &board)
+    {
+        for(int i=0;i<9;i++)
+        {
+            //check in same row;
+            if(board[row][i]==c)
+                return false;
+            //check in same column
+            if(board[i][col]==c)
+                return false;
+            //check in 3x3
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+                return false;
+        }
+        return true;
+    }
+    
+    bool solve(vector<vector<char>> &board)
+    {
+        for(int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board[0].size();j++)
+            {
+                if(board[i][j]=='.')
+                {
+                    for(char c='1';c<='9';c++)
+                    {
+                        if(valid(i,j,c,board))
+                        {
+                            board[i][j]=c;
+                            if(solve(board))
+                                return true;
+                            else
+                                board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) 
+    {
+        solve(board);
+    }
+    
+};
