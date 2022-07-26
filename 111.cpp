60de01431701f2a7a57dd3b8611d2a9dac6bfabf
Anubhav0004 <anubhav.goyal1029@gmail.com>
2022-07-27 04:27:32 +0530

    TREE

---
+++ 111.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) 
+    {
+        if(!root)
+            return 0;
+        int left=minDepth(root->left);
+        int right=minDepth(root->right);
+        return 1+(min(left,right)?min(left,right):max(left,right));
+    }
+};
