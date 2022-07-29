848f23d528015fe016e9dd88a027eb881cfc6a94
Anubhav0004 <anubhav.goyal1029@gmail.com>
2022-07-30 03:24:51 +0530

    MATRIX

---
+++ 48.cpp
@@ -0,0 +1,11 @@
+class Solution {/////////////////////Take transpose than reverse each row///////////// 
+public:
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        for(int i=0;i<matrix.size();i++)
+            for(int j=0;j<i;j++)
+                swap(matrix[i][j],matrix[j][i]);
+        for(int i=0;i<matrix.size();i++)   
+            reverse(matrix[i].begin(),matrix[i].end());
+    }
+};
