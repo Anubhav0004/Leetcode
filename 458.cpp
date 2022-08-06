4541084c5fc85ae9ef8676a00a0ac0e4e39502ce
Anubhav0004 <anubhav.goyal1029@gmail.com>
2022-08-07 02:29:25 +0530

    MATH

---
+++ 458.cpp
@@ -0,0 +1,16 @@
+class Solution { /////////////100% solution////////////////////
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) 
+    {
+        if(buckets-- ==1)
+            return 0;
+        int base = minutesToTest/minutesToDie +1;
+        int res=0;
+        while(buckets>0)
+        {
+            buckets=buckets/base;
+            res+=1;
+        }
+        return res;
+    }
+};
